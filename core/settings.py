85907c0eb8a09ece89cebf5bac11d4bc5858e212
Jahidul Hassan Reshad <reshad22205341197@diu.edu.bd>
2024-09-26 21:12:54 +0600

    allowed host update

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k2)=0mh^yml21*3yu@mmxj+y)$r^$!+=e+e%t(bt+)z)g34emf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
