2d939659eebf7bb36265f663ad40919d20ef9913
Jahidul Hassan Reshad <reshad22205341197@diu.edu.bd>
2024-10-01 20:03:59 +0600

    https // update

--- core/settings.py
+++ core/settings.py
@@ -148,6 +148,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    'http://slingringui-production.up.railway.app',
+    'https://slingringui-production.up.railway.app',
 
 ]
